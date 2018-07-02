71a4c2ef62529c5ece1c52bdf4f94e2bb5386004
jessieshao <505133853@qq.com>
2018-07-02 14:24:39 +0800

    commit

---
+++ main.js
@@ -0,0 +1,39 @@
+let n
+inStart()
+setInterval(()=>{ 
+    makeLeave(getImage(n))
+    .one('transitionend',(a)=>{
+        makeEnter($(a.currentTarget))
+    })
+    makeCurrent(getImage(n+1))
+    n += 1
+},2000)
+
+//功能函数
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+function inStart(){
+    n=1
+    $(`.images > img:nth-child(${x(n)})`).addClass('current')
+    .siblings().addClass('enter')    
+}
+function x(n){
+    if (n>3){
+        n = n%3
+        if(n===0){
+            n = 3
+        }
+    }
+    return n
+}
+function makeCurrent($node){
+    $node.removeClass('leave enter').addClass('current')
+}
+function makeLeave($node){
+    $node.removeClass('current enter').addClass('leave')
+    return $node
+}
+function makeEnter($node){
+    $node.removeClass('leave current').addClass('enter')
+}
